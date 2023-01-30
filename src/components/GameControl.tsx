bc3cda52389a5f4b9f36f1fd2b427bf4836c11f9
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-31 01:07:59 +0300

    feat: implement game control

--- src/components/GameControl.tsx
+++ src/components/GameControl.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import GameHandlers from '../socket/gameHandlers';
+import socket, { SocketControl } from '../socket/socket';
 
 const GameControl = () => {
-  console.log(1);
+  const navigate = useNavigate();
+  const [users, setUsers] = useState<string[]>([]);
+  const uniqueKey = () => Math.random();
+  const exit = async () => {
+    SocketControl.disconnect();
+    navigate('/');
+  };
 
-  return (<div>1</div>);
+  socket.on('player:online', (userName) => {
+    setUsers([...users, userName]);
+  });
+
+  const getName = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { room } = (e.target as HTMLButtonElement).dataset;
+
+    GameHandlers.startGame(room as string);
+    localStorage.setItem('plaing', room as string);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={exit}>exit</button>
+      <div>
+        {users.map((user) => (
+          <button
+            style={{ marginTop: '15px', width: '40px', height: '30px' }}
+            type="button"
+            data-room={user}
+            key={uniqueKey()}
+            onClick={getName}
+          >
+            {user}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GameControl;
